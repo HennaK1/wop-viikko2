a907b3e53bfe3e8922a0bc9271df7dd1d83b88db
Henna Koli <henna@Henna-MacBook-Pro.local>
2021-11-24 12:21:08 +0200

    gps

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,27 +2,59 @@
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const { httpError } = require('../utils/errors');
+const { validationResult } = require('express-validator');
+const { addUser } = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(12);
 
 const login = (req, res, next) => {
-  // TODO: add passport authenticate
-  passport.authenticate('local', { session: false }, (err, user, info) => {
-    console.log('login info', err, user, info);
-    if(err || !user) {
-        next(httpError('Invalid username/password', 400));
-        return;
-    }
-    req.login(user, { session: false }, (err) => {
-        if (err){
-            next(httpError('Login error', 400));
+    // TODO: add passport authenticate
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+        console.log('login info', err, user, info);
+        if (err || !user) {
+            next(httpError('Invalid username/password', 400));
             return;
         }
-        delete user.password;
-        const token = jwt.sign(user, process.env.JWT_SECRET);
-        return res.json({ user, token });
-    });
-  })(req, res, next);
+        req.login(user, { session: false }, (err) => {
+            if (err) {
+                next(httpError('Login error', 400));
+                return;
+            }
+            delete user.password;
+            const token = jwt.sign(user, process.env.JWT_SECRET);
+            return res.json({ user, token });
+        });
+    })(req, res, next);
+};
+
+const user_post = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log('user_post validation', errors.array());
+        next(httpError('Invalid data', 400));
+        return;
+    }
+    try {
+        console.log('Lomakkeesta', req.body);
+        const { name, email, passwd } = req.body;
+        // hash password
+        const hash = bcrypt.hashSync(passwd, salt);
+        const tulos = await addUser(name, email, hash, next);
+        if (tulos.affectedRows > 0) {
+            res.json({
+                message: "user added",
+                user_id: tulos.insertId,
+            });
+        } else {
+            next(httpError('No user inserted', 400));
+        }
+    } catch (e) {
+        console.log('user_get error', e.message);
+        next(httpError('internal server error', 500));
+    }
 };
 
 module.exports = {
-  login,
+    login,
+    user_post,
 };
